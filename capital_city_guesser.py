85ddacb8437838010828a46c07d92ce86bf8ba20
Benjamin Salon <detroit9@azet.sk>
2022-04-10 00:07:10 +0200

    Filtering continents, coutries and capital cities from continents

--- capital_city_guesser.py
+++ capital_city_guesser.py
@@ -13,12 +13,27 @@ class GuesserApp:
     def __initialize_continents_dict(self):
         continents = self.__get_countries_by_continent()
         continents_with_capitals = self.__add_capitals_to_continent_countries(continents)
-        self.continents = list(continents_with_capitals)
+        continents_dict = self.__create_continent_countries_dict(continents_with_capitals)
+        self.continents = continents_dict
 
 
     def __get_countries_by_continent(self):
         all_continents = self.client.service.ListOfCountryNamesGroupedByContinent()
         return filter(lambda c: "Antarctica" not in c["Continent"]["sName"], all_continents)
+
+
+    def __create_continent_countries_dict(self, continents_with_capitals):
+        continents_dict = {}
+        for continent in continents_with_capitals:
+            continent_name = continent['Continent']['sName'].strip()
+            continents_dict[continent_name] = []
+
+            for country in continent['CountryCodeAndNames']['tCountryCodeAndName']:
+                country_name = country['sName']
+                country_capital = country['sCapitalCity']
+                continents_dict[continent_name].append((country_name, country_capital))
+
+        return continents_dict
 
 
     def __add_capitals_to_continent_countries(self, continents):
