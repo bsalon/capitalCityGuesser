22304e8b6b53598765fa1d3e954529ea89334488
Benjamin Salon <detroit9@azet.sk>
2022-04-16 14:15:47 +0200

    Using username instead of name where possible

--- GUIManager.py
+++ GUIManager.py
@@ -26,31 +26,31 @@ class GUIManager(tkinter.Tk):
         st_frm = self.startup_frame
 
         _ = ttk.Button(st_frm, text="Show leaderboard", style="leaderboard.TButton").pack(expand=True, fill="both")
-        _ = ttk.Label(st_frm, text="Enter your name:", style="name.TLabel").pack(fill="x")
+        _ = ttk.Label(st_frm, text="Enter your name:", style="username.TLabel").pack(fill="x")
 
-        self.name = tkinter.StringVar()
-        self.name_entry = ttk.Entry(st_frm, textvariable=self.name)
-        self.name_entry.pack(fill="x")
+        self.username = tkinter.StringVar()
+        self.username_entry = ttk.Entry(st_frm, textvariable=self.username)
+        self.username_entry.pack(fill="x")
 
-        self.name_confirm_button = ttk.Button(st_frm, text="Confirm", style="confirm_name.TButton", command=self.__get_user_name)
-        self.name_confirm_button.pack(fill="x")
+        self.username_confirm_button = ttk.Button(st_frm, text="Confirm", style="confirm_username.TButton", command=self.__get_user_name)
+        self.username_confirm_button.pack(fill="x")
 
-        self.name.trace("w", self.__on_name_entry_trace)
-        self.name.set("")
+        self.username.trace("w", self.__on_username_entry_trace)
+        self.username.set("")
 
 
-    def __on_name_entry_trace(self, *args):
-        new_state = "disabled" if self.name.get() == "" else "normal"
-        self.name_confirm_button.config(state=new_state)
+    def __on_username_entry_trace(self, *args):
+        new_state = "disabled" if self.username.get() == "" else "normal"
+        self.username_confirm_button.config(state=new_state)
 
 
     def __style_configuration(self):
         self.ttk_style = ttk.Style()
         self.ttk_style.configure("leaderboard.TButton", font=("Helvetica", 36))
-        self.ttk_style.configure("name.TLabel", font=("Times New Roman", 18))
-        self.ttk_style.configure("confirm_name.TButton", font=("Helvetica", 18))
+        self.ttk_style.configure("username.TLabel", font=("Times New Roman", 18))
+        self.ttk_style.configure("confirm_username.TButton", font=("Helvetica", 18))
 
 
     def __get_user_name(self):
         self.startup_frame.pack_forget()
-        return self.name.get()
+        return self.username.get()
