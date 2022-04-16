43fe9226888aed5956d8e6cf52e78b4a51826bc7
Benjamin Salon <detroit9@azet.sk>
2022-04-16 17:38:39 +0200

    Creating frame for guessing the capital city of a country

--- GUIManager.py
+++ GUIManager.py
@@ -21,6 +21,10 @@ class GUIManager(tkinter.Tk):
         self.startup_frame.pack(expand=True, fill=tkinter.BOTH)
 
 
+    def pack_guess_frame(self):
+        self.guess_frame.pack(expand=True, fill=tkinter.BOTH)
+
+
     def __create_startup_frame(self):
         self.startup_frame = ttk.Frame(self)
         st_frm = self.startup_frame
@@ -39,6 +43,21 @@ class GUIManager(tkinter.Tk):
         self.username.set("")
 
 
+    def __create_guess_frame(self):
+        self.guess_frame = ttk.Frame(self)
+        gu_frm = self.guess_frame
+
+        index = tkinter.IntVar()
+        index.set(-1)
+
+        _ = ttk.Label(gu_frm, text=f"What is the capital city of", style="question.TLabel", anchor="center").pack(expand=True, fill="x")
+        _ = ttk.Radiobutton(gu_frm, text="test", style="guess.TRadiobutton", variable=index, value=0).pack(fill="x")
+        _ = ttk.Radiobutton(gu_frm, text="test", style="guess.TRadiobutton", variable=index, value=1).pack(fill="x")
+        _ = ttk.Radiobutton(gu_frm, text="test", style="guess.TRadiobutton", variable=index, value=2).pack(fill="x")
+        _ = ttk.Radiobutton(gu_frm, text="test", style="guess.TRadiobutton", variable=index, value=3).pack(fill="x")
+        _ = ttk.Button(gu_frm, text="confirm", style="confirm_username.TButton").pack(fill="x")
+
+
     def __on_username_entry_trace(self, *args):
         new_state = "disabled" if self.username.get() == "" else "normal"
         self.username_confirm_button.config(state=new_state)
@@ -48,9 +67,21 @@ class GUIManager(tkinter.Tk):
         self.ttk_style = ttk.Style()
         self.ttk_style.configure("leaderboard.TButton", font=("Helvetica", 36))
         self.ttk_style.configure("username.TLabel", font=("Times New Roman", 18))
+        self.ttk_style.configure("question.TLabel", font=("Times New Roman", 24))
         self.ttk_style.configure("confirm_username.TButton", font=("Helvetica", 18))
+        self.ttk_style.configure("guess.TRadiobutton", font=("Helvetica", 18),
+                                                       indicatorrelief=tkinter.FLAT,
+                                                       indicatormargin=-1,
+                                                       indicatordiameter=-1,
+                                                       relief=tkinter.RAISED,
+                                                       focusthickness=5,
+                                                       padding=5)
+        self.ttk_style.map("guess.TRadiobutton",
+                           background=[("selected", "white"), ("active", "#ececec")])
 
 
     def __get_username(self):
         self.startup_frame.pack_forget()
+        self.__create_guess_frame()
+        self.pack_guess_frame()
         return self.username.get()
