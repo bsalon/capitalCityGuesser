f4ae8f0bf3fff4a46a89b595113256e8734336c0
Benjamin Salon <detroit9@azet.sk>
2022-04-28 23:46:25 +0200

    Creating Country entity storing country information

---
+++ data/Entities/Country.py
@@ -0,0 +1,6 @@
+class Country:
+    def __init__(self, country_id, name, continent, capital_city):
+        self.country_id = country_id
+        self.name = name
+        self.continent = continent
+        self.capital_city = capital_city
