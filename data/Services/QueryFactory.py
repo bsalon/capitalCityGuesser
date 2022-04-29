45020da22bf6f41b27fbbb757aaaea87d69fc2dc
Benjamin Salon <detroit9@azet.sk>
2022-04-29 22:38:21 +0200

    Removing auto-filled columns from insert query

--- data/Services/QueryFactory.py
+++ data/Services/QueryFactory.py
@@ -5,9 +5,14 @@ class QueryFactory:
 
 
     def create_insert_query(table_name, column_tuples):
-        table_column_names = QueryFactory.__parse_column_names(column_tuples)
-        placeholders = QueryFactory.__create_placeholders(column_tuples)
+        nongenerated_column_tuples = QueryFactory.__filter_generated_columns(column_tuples)
+        table_column_names = QueryFactory.__parse_column_names(nongenerated_column_tuples)
+        placeholders = QueryFactory.__create_placeholders(nongenerated_column_tuples)
         return f"INSERT INTO {table_name} {table_column_names} VALUES {placeholders}"
+
+
+    def __filter_generated_columns(column_tuples):
+        return [(col, t_col) for col, t_col in column_tuples if ("AUTO" not in t_col and "GENERATED" not in t_col)]
 
 
     def __create_placeholders(columns):
