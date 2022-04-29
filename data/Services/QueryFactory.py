d415d19245598162e6c3a5bd2ad39c8591b902e5
Benjamin Salon <detroit9@azet.sk>
2022-04-29 23:39:09 +0200

    Removing parenthesis concatenation from parsing methods

--- data/Services/QueryFactory.py
+++ data/Services/QueryFactory.py
@@ -1,18 +1,18 @@
 class QueryFactory:
     def create_create_table_query(table_name, column_tuples):
         table_columns = QueryFactory.__parse_column_tuples(column_tuples)
-        return f"CREATE TABLE {table_name} {table_columns}"
+        return f"CREATE TABLE {table_name} ({table_columns})"
 
 
     def create_insert_query(table_name, column_tuples):
         nongenerated_column_tuples = QueryFactory.__filter_generated_columns(column_tuples)
         table_column_names = QueryFactory.__parse_column_names(nongenerated_column_tuples)
         placeholders = QueryFactory.__create_placeholders(nongenerated_column_tuples)
-        return f"INSERT INTO {table_name} {table_column_names} VALUES {placeholders}"
+        return f"INSERT INTO ({table_name}) {table_column_names} VALUES ({placeholders})"
 
 
     def create_select_query(table_name, column_tuples):
-        table_column_names = QueryFactory.__parse_column_names(column_tuples)[1:-1]
+        table_column_names = QueryFactory.__parse_column_names(column_tuples)
         return f"SELECT {table_column_names} FROM {table_name}"
 
 
@@ -21,12 +21,12 @@ class QueryFactory:
 
 
     def __create_placeholders(columns):
-        return "(" + ", ".join(["%s"] * len(columns)) + ")"
+        return ", ".join(["%s"] * len(columns))
 
 
     def __parse_column_names(column_tuples):
-        return "(" + ", ".join([col_name for col_name, col_type in column_tuples]) + ")"
+        return ", ".join([col_name for col_name, col_type in column_tuples])
 
 
     def __parse_column_tuples(column_tuples):
-        return "(" + ", ".join([col_name + " " + col_type for col_name, col_type in column_tuples]) + ")"
+        return ", ".join([col_name + " " + col_type for col_name, col_type in column_tuples])
