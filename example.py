72d69d5095056ecf117703d7b770c530852608f4
Benjamin Salon <detroit9@azet.sk>
2022-04-29 22:51:21 +0200

    Creating example of safe-adding the countries into the countries table

--- example.py
+++ example.py
@@ -5,8 +5,12 @@ from data.Services.CountryService import CountryService
 
 def main():
     country_client = CountryClient("http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL")
+    countries = country_client.countries
+
     country_service = CountryService()
-    print(list(country_client.countries))
+    if not country_service.has_at_least_rows(100):
+        country_service.clear_table()
+        country_service.insert_countries(list(countries))
 
 if __name__ == "__main__":
     main()
